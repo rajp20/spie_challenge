a13fe5f642655d6bd08fc4f6d8d510a0ce5ed152
Raj Patel <rajpatel0820@gmail.com>
2019-04-25 16:28:59 -0600

    working on loading data

--- data_loader.py
+++ data_loader.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 class BreastPathQDataSet(Dataset):
     def __init__(self, split = "train"):
-        self.images = "TODO"
+        self.labels = {}
         if split is "train":
             # Set up training data
             self.dataset_path = "./datasets/breastpathq/datasets/train/"
-            self.label_path = "./datasets/breaspathq/datasets/train_labels.csv"
+            self.label_path = "./datasets/breastpathq/datasets/train_labels.csv"
         elif split is "val":
             self.dataset_path = "./datasets/breastpathq/datasets/validation/"
             self.label_path = "./datasets/breastpathq-test/val_labels.csv"
@@ -17,11 +17,13 @@ class BreastPathQDataSet(Dataset):
 
         if split is "train" or "val":
             label_csv = pd.read_csv(self.label_path, skiprows=1)
-            for line in label_csv:
-                print(line)
+            for index, row in label_csv.iterrows():
+                key = str(int(row[0])) + "_" + str(int(row[1]))
+                self.labels[key] = row[2]
+            print(len(self.labels))
 
     def __getitem__(self, item):
         return item
 
     def __len__(self):
-        return len(self.images)
+        return len(self.labels)
--- main.py
+++ main.py
@@ -18,27 +18,24 @@ def main():
     :return:
     """
     define_gpu()
-    print("Here")
     training_data = BreastPathQDataSet(split="train")
     del training_data
 
 
-def define_gpu(minimum_memory_mb = 3800):
-    gpu_to_use = None
+def define_gpu(minimum_memory_mb = 1800):
+    gpu_to_use = 0
     try:
         print('GPU already assigned before: ' + str(os.environ['CUDA_VISIBLE_DEVICES']))
         return
     except:
         pass
     torch.cuda.empty_cache()
-    if gpu_to_use is None:
-        print('Could not find any GPU available with the required free memory of ' +str(minimum_memory_mb) + 'MB. Please use a different system for this assignment.')
-    else:
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_to_use)
-        print('Chosen GPU: ' + str(gpu_to_use))
-        x = torch.rand((256,1024,minimum_memory_mb-500)).cuda()
-        x = torch.rand((1,1)).cuda()
-        del x
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_to_use)
+    print('Chosen GPU: ' + str(0))
+    x = torch.rand((256,1024,minimum_memory_mb-500)).cuda()
+    del x
+    x = torch.rand((1,1)).cuda()
+    del x
 
 
 main()
