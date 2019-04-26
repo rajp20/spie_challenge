e2b2636bfa1a1bfd72a6769e1e2386a14f430c30
Raj Patel <rajpatel0820@gmail.com>
2019-04-26 15:33:28 -0600

    another fix

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 # import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import sys
-from sklearn.metrics import roc_curve, auc
 import copy
 import torchvision.models as models
 from data_loader import BreastPathQDataSet
