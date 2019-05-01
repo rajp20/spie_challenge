00b4a45598f229cca35a47eed69dcdbaa0a324d5
Raj Patel <rajpatel0820@gmail.com>
2019-05-01 00:38:52 -0600

    more graph fixes

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def main():
             print("Learning Rate:", lr)
             trained_model, losses, scores = utils.train(model, epochs, batch, criterion, optimizer)
             lr_string = '%.2E' % Decimal(str(lr))
-            label = "Learning Rate: " + lr_string + ", Batch Size: " + str(batch)
+            label = "LR: " + lr_string + ", Batch: " + str(batch)
             losses_figure_ax.plot(range(0, len(losses)), losses, label=label)
             scores_figure_ax.plot(range(0, len(scores)), scores, label=label)
 
@@ -109,7 +109,7 @@ def main():
 
     scores_figure_ax.set_title("Scores vs. Epochs")
     scores_figure_ax.set_xlabel("Epochs")
-    scores_figure_ax.set_ylabel("Losses")
+    scores_figure_ax.set_ylabel("Scores")
     scores_figure_ax.legend()
     scores_figure.savefig("./graphs/" + model_type + "_" + optimizer_type + "_Scores_" + str(epochs) + "e.png")
 
