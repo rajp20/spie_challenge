1d68130ef5696504eaabb54813236d61c492f0cb
Raj Patel <rajpatel0820@gmail.com>
2019-05-01 00:24:31 -0600

    added mroe channels

--- improved_convnet.py
+++ improved_convnet.py
@@ -4,27 +4,27 @@ import torch
 class ImprovedConvNet(torch.nn.Module):
     def __init__(self):
         super(ImprovedConvNet, self).__init__()
-        self.input = torch.nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3)
+        self.input = torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
 
-        self.block_1_1 = torch.nn.Conv2d(in_channels=8, out_channels=8, kernel_size=1)
-        self.block_1_2 = torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3)
-        self.block_1_3 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+        self.block_1_1 = torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=1)
+        self.block_1_2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.block_1_3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
 
-        self.block_2_1 = torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=1)
-        self.block_2_2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        self.block_2_3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+        self.block_2_1 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1)
+        self.block_2_2 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3)
+        self.block_2_3 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3)
 
-        self.block_3_1 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1)
-        self.block_3_2 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3)
-        self.block_3_3 = torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3)
+        self.block_3_1 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1)
+        self.block_3_2 = torch.nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3)
+        self.block_3_3 = torch.nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=3)
 
-        self.block_4_1 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1)
-        self.block_4_2 = torch.nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3)
-        self.block_4_3 = torch.nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3)
+        self.block_4_1 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1)
+        self.block_4_2 = torch.nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3)
+        self.block_4_3 = torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3)
 
-        self.block_5_1 = torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=1)
-        self.block_5_2 = torch.nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3)
-        self.block_5_3 = torch.nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3)
+        self.block_5_1 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1)
+        self.block_5_2 = torch.nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3)
+        self.block_5_3 = torch.nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3)
 
         self.fc_1 = torch.nn.Linear(in_features=1152, out_features=512)
         self.fc_2 = torch.nn.Linear(in_features=512, out_features=256)
