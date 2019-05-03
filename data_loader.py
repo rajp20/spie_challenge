6cef824a520126bc7cb60f0a58ff82e029101678
Raj Patel <rajpatel0820@gmail.com>
2019-05-02 18:26:28 -0600

    last test

--- data_loader.py
+++ data_loader.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import os
 import torch
+import random
 
 
 class BreastPathQDataSet(Dataset):
@@ -25,6 +26,54 @@ class BreastPathQDataSet(Dataset):
         for index, row in label_csv.iterrows():
             key = str(int(row[0])) + "_" + str(int(row[1]))
             self.dataset.append({"image": key, "label": row[2], "slide": int(row[0]), "rid": int(row[1])})
+
+    # apply transoforms to all tensors in list x
+    def _iterate_transforms(self, transform, x):
+        for i, xi in enumerate(x):
+            x[i] = transform(x[i])
+        return x
+
+    # redefining composed transform so that it uses the _iterate_transforms function
+    class Compose(object):
+        def __init__(self, transforms):
+            self.transforms = transforms
+
+        def __call__(self, x):
+            for transform in self.transforms:
+                x = self._iterate_transforms(transform, x)
+            return x
+
+    # class to rerandomize the vertical flip transformation
+    class RandomVerticalFlipGenerator(object):
+        def __call__(self, img):
+            self.random_n = random.uniform(0, 1)
+            return img
+
+    # class to perform vertical flip using randomization provided by gen
+    class RandomVerticalFlip(object):
+        def __init__(self, gen):
+            self.gen = gen
+
+        def __call__(self, img):
+            if self.gen.random_n < 0.5:
+                return torch.flip(img, [2])
+            return img
+
+    # class to rerandomize the horizontal flip transformation
+    class RandomHorizontalFlipGenerator(object):
+        def __call__(self, img):
+            self.random_n = random.uniform(0, 1)
+            return img
+
+    # class to perform horizontal flip using randomization provided by gen
+    class RandomHorizontalFlip(object):
+        def __init__(self, gen):
+            self.gen = gen
+
+        def __call__(self, img):
+            if self.gen.random_n < 0.5:
+                return torch.flip(img, [1])
+            return img
 
     def __getitem__(self, index):
         indexed_label = self.dataset[index]['label']
--- simple_convnet.py
+++ simple_convnet.py
@@ -10,7 +10,7 @@ class SimpleConvNet(torch.nn.Module):
         self.convolution_layer_2 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4)
         self.convolution_layer_3 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4)
         self.convolution_layer_4 = torch.nn.Conv2d(in_channels=64, out_channels=32, kernel_size=4)
-        self.convolution_layer_5 = torch.nn.Conv2d(in_channels=32, out_channels=16, kernel_size=4)
+        self.convolution_layer_5 = torch.nn.Conv2d(in_channels=32, out_channels=1, kernel_size=4)
         self.fully_connected_layer = torch.nn.Linear(in_features=2704, out_features=1)
 
     def forward(self, x):
@@ -31,7 +31,7 @@ class SimpleConvNet(torch.nn.Module):
         x = self.maxpooling_layer(x)
 
         # flattening the tensor so that it can serve as input to a linear layer
-        x = x.view(x.size(0), -1)
-        x = self.fully_connected_layer(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.fully_connected_layer(x)
 
         return x
